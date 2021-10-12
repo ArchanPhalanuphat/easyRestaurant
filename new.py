9aa9d9bc8dc248bd70582f82ddba511a54dbe713
pangpntt <pang.pntt@gmail.com>
2021-10-12 21:40:28 +0700

    งง1

--- new.py
+++ new.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 eiei = "5555"
 print(eiei)
-=======
 aa = "aa"
 print(aa)
->>>>>>> 00f5029c724909667b55b0a7baa4183280bc1c93
+txt = "งองงงงง"
+print(txt)
--- new2.py
+++ new2.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 print("hahahah")
-=======
 num = 1
 print(num)
->>>>>>> 79d93b7a15c3f5ecf05938fc2c632688fed41425
